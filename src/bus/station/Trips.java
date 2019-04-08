ef52b890bd96101df54c8afb7b40799f44ab3608
MirnaNasrallah <48534649+MirnaNasrallah@users.noreply.github.com>
2019-04-08 23:32:34 +0200

    Bus station demo

---
+++ src/bus/station/Trips.java
@@ -0,0 +1,12 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bus.station;
+
+import java.io.File;
+public class Trips { 
+     
+   File trips = new File("");
+}
